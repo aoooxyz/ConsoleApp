5d371b42db2b42cc6e058a81b5850e58d32548c0
khoo <auld2002@gmail.com>
2025-04-06 23:35:23 +0800

    多态

--- Program.cs
+++ Program.cs
@@ -3,26 +3,24 @@ namespace consoleapp  //定义命名空间consoleapp
 {
     class Demo  //定义Demo类
 	{
-        static void Main(string[] args) //主方法，程序的入口
+        void print(int i)
 		{
-			Test test1 = new Test();  //创建Test类的实例对象test1
-			test1.setStr("奈非天");  //调用test1的setStr()方法(赋值为"奈非天")
-			Test.getStr();  //调用静态方法getStr()输出静态字段str
-			Test test2 = new Test();  //创建Test类的实例对象test2
-			test2.setStr("莉莉丝");  //调用test2的setStr()方法(赋值为"莉莉丝")
-			Test.getStr();  //调用静态方法getStr()输出静态字段str
+			Console.WriteLine("打印int类型的数据：{0}",i);
 		}
-	}
-	public class Test  //定义Test类
-	{
-	    public static string str;  //定义一个静态字段str,所有Test类的实例共享这个变量
-		public void setStr(string s)  //定义实例方法setStr，用于修改静态字段str的值
+		void print(double d)
 		{
-		    str = s;  //直接修改静态字段str
+			Console.WriteLine("打印double类型的数据：{0}",d);
 		}
-		public static void getStr()  //定义实例方法getStr，用于输出静态字段str的值
+		void print(string s)
 		{
-		    Console.WriteLine(str);  //输出静态字段str的值
+			Console.WriteLine("打印string类型的数据：{0}",s);
+		}
+		static void Main(string[] args)
+		{
+			Demo p = new Demo();
+			p.print(111);
+			p.print("hello world");
+			p.print(3.1415926);
 		}
 	}
 }
